a8bb48785e4b011616388a8f470e8cf0cce2c573
jsamp90 <jeremysampson1@gmail.com>
2017-05-16 09:35:41 +0100

    Made switches and Video BG

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ console.log('Hello world!');
 
 $(document).ready(function() {
 	console.log('ready');
-	var headerHeight = $('header').height():
+	var headerHeight = $('header').height();
 	console.log(headerHeight);
 });
 
@@ -11,5 +11,32 @@ $(window).resize(function() {
 });
 
 $(window).scroll(function() {
+	var scrollAmount = $(window).scrollTop();
+	if (scrollAmount > headerHeight) {
+		$('header').css({
+			'position': 'fixed'
+		})
+	} 
 	console.log('scroll');
 });
+
+$( ".switch a" ).click(function(event) {
+	event.preventDefault();
+	var clickedOption = $(this).attr("class");
+  	if(clickedOption=='topics'){
+  		$('.headercontent.lawyers').hide();
+  		$('.headercontent.topics').show();
+  	}
+
+  	if(clickedOption=='lawyers'){
+  		$('.headercontent.topics').hide();
+  		$('.headercontent.lawyers').show();
+  	}
+
+  	// add active class
+  	$( ".switch a" ).removeClass( "active" );
+  	$( ".switch a."+clickedOption ).addClass( "active" );
+
+
+});
+
